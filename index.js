39014bc4cf80155fe96ed45d802d88867bd4a3d4
Ryan White <ryanwhite04@gmail.com>
2017-01-02 06:56:23 +0800

    Ignore instruments when installed via npm

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+const parser = require('parser');
+const families = require('./families');
+const names = require('./names');
 
 module.exports = {
-  instruments: require('./instruments'),
-  parser: require('note-parser'),
+  parser, families,
+  instruments: name.map((name, midi) => {
+    midi, name,
+    family: families[~~(midi / 16)],
+    path: `instruments/${name}`,
+    mp3: `instruments/${name}/mp3.json`,
+    ogg: `instruments/${name}/ogg.json`,
+    notes: {
+      first: 21,
+      last: 109,
+    },
+  }),
 }
